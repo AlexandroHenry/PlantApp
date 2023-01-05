d7cd7ee3803acbb0b4a2d01e0c9624e24d07e004
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-05 13:21:32 +0900

    Initial Commit

---
+++ PlantApp/PlantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantAppApp.swift
+//  PlantApp
+//
+//  Created by Seungchul Ha on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct PlantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
